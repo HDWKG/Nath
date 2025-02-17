53d6b983d94944e3e9d0dfada67e6e637caf51f1
HDWKG <73515921+HDWKG@users.noreply.github.com>
2025-02-17 20:16:37 +0700

    fin

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-menu");
+    const body = document.body;
     menu.classList.toggle("open");
     icon.classList.toggle("open");
+    body.classList.toggle('menu-open');
 }
 document.querySelector('.hamburger-icon').addEventListener('click', function() {
     this.classList.toggle('open');
